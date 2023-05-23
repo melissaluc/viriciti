1a440f25cc4dcbf91dc81b111e71be883a954852
Melissa <Melissa.Luc@metrolinx.com>
2023-05-23 11:28:44 -0400

    Add constant.py to load credentials from .env file to get account USERNAME & PASSWORD

--- helpers/constants.py
+++ helpers/constants.py
@@ -1,3 +1,15 @@
 import os
+from dotenv import load_dotenv
 
-os.environ['']=   
+curr_dir = os.path.dirname(__file__)
+dotenv_path = os.path.realpath(
+    os.path.join(curr_dir, '..', 'secrets', 'credentials.env')
+    )
+
+load_dotenv(dotenv_path=dotenv_path)
+
+# Account Login Credentials
+USERNAME = os.getenv('ACCOUNT_USERNAME')
+PASSWORD = os.getenv('ACCOUNT_PASSWORD')
+
+# Database Connection Credentials
